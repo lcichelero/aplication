3f0f38697ec09034c55ce6e8e2c0def385ad5760
Leandro Cichelero <lcichelero@ciss.com.br>
2016-06-01 19:28:01 -0300

    Criado uma estrutura de repeticao

--- js/controller/MeuController.js
+++ js/controller/MeuController.js
@@ -1,4 +1,26 @@
 //A Variavel myApp foi criada no arquivo myApp.js
 myApp.controller('MeuController', function($scope) {
-	console.log('Start!');
+	$scope.dados = [{
+		id : 1,
+		nome : 'Leandro Cichelero',
+		telefone : '(46) 1111-7989',
+		email : 'lcichelero@ciss.com.br'
+	},{
+		id : 2,
+		nome : 'Kelly Meurer',
+		telefone : '(46) 2222-73349',
+		email : 'kelly@ciss.com.br'
+	},{
+		id : 3,
+		nome : 'Pedro Lucas M. Cichelero',
+		telefone : '(46) 3333-73349',
+		email : 'pedro@gmail.com.br'
+	},{
+		id : 4,
+		nome : 'Sandrinha',
+		telefone : '(46) 4444-73349',
+		email : 'sandrinha@gmail.com.br'
+	}];
+
+	console.log($scope.dados);
 })
